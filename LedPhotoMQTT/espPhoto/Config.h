8230392de62727d502d9783ed7276f5f874eb45b
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-10-28 18:49:20 +0800

    adding code Config.h

---
+++ LedPhotoMQTT/espPhoto/Config.h
@@ -0,0 +1,15 @@
+String ssidAP = "ESP_WIFI"; 
+String passwordAP = "ESP8266123";
+
+//char* ssidCLI = "YOUR_SSID"; // имя контроллера и точки доступа
+//char* passwordCLI = "YOUR_PASS"; // пароль точки доступа
+
+char* ssidCLI = "TP-Link_A2E2";
+char* passwordCLI = "4561127845";
+
+char* mqtt_broker = "broker.emqx.io";
+
+const int mqtt_port = 1883;
+
+const int led = 2;
+const char* sensor_topic = "home/light_sensor";
