124668e1c3735ea8486b4de5e3891bf94897647c
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-10-28 18:41:49 +0800

    adding code Config.h

---
+++ LedPhotoMQTT/espLed/Config.h
@@ -0,0 +1,15 @@
+String ssidAP = "ESP_WIFI"; 
+String passwordAP = "ESP8266123";
+
+//char* ssidCLI = "YOUR_SSID"; // имя контроллера и точки доступа
+//char* passwordCLI = "YOUR_PASS"; // пароль точки доступа
+
+char* ssidCLI = "iPhone (Кирилл)";
+char* passwordCLI = "123456789";
+
+char* mqtt_broker = "broker.emqx.io";
+
+const int mqtt_port = 1883;
+
+const int led = 2;
+const char* sensor_topic = "home/light_sensor";
