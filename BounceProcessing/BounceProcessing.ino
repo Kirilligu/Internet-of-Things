f68ff7983e951a0ca09c903c574372aa710932f5
smmtoper <138273906+smmtoper@users.noreply.github.com>
2025-02-04 22:17:41 +0800

    Update BounceProcessing.ino

--- BounceProcessing/BounceProcessing.ino
+++ BounceProcessing/BounceProcessing.ino
@@ -1,6 +1,7 @@
 #define BUTTON_PIN 2 
 #define BOUNCE_TIME 50
 #define PRESSED HIGH
+
 volatile long int press_time = 0;
 volatile bool pressed_candidate = false;
 volatile long int hold_time = 0;
@@ -11,22 +12,39 @@ bool button_pressed = false;
 void setup() {
   pinMode(BUTTON_PIN, INPUT);
   Serial.begin(9600);
-  attachInterrupt(0, process_button_click, RISING);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), process_button_click, CHANGE);
 }
+
 void loop() {
-  if(report_to_user == true) {
-      Serial.println("Press candidate");
+  if (report_to_user) {
+    Serial.print("Press count: ");
+    Serial.println(press_count);
+    Serial.print("Hold time: ");
+    Serial.println(hold_time);
     report_to_user = false;
   }
-  // debounce and other code here
-  // if real press -> press_count++;
-  // check releaased only after pressed
 }
+
 void process_button_click() {
-  if (pressed_candidate == false) {
-    press_time = millis();
-    pressed_candidate = true;
-    report_to_user = true;
-    hold_time = 0;
+  static long int last_interrupt_time = 0;
+  long int interrupt_time = millis();
+
+  if (interrupt_time - last_interrupt_time < BOUNCE_TIME) {
+    return;
+  }
+  last_interrupt_time = interrupt_time;
+
+  if (digitalRead(BUTTON_PIN) == PRESSED) {
+    if (!pressed_candidate) {
+      press_time = interrupt_time;
+      pressed_candidate = true;
+    }
+  } else {
+    if (pressed_candidate) {
+      hold_time = interrupt_time - press_time;
+      press_count++;
+      pressed_candidate = false;
+      report_to_user = true;
+    }
   }
 }
