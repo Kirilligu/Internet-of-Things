6a0de7cca5ca0d298087f3e7fe58d915bc28cee7
smmtoper <138273906+smmtoper@users.noreply.github.com>
2025-02-13 14:43:16 +0800

    ready-made 0, 1 and 2

--- ButtonMatrix/ButtonMatrix.ino
+++ ButtonMatrix/ButtonMatrix.ino
@@ -1,71 +1,136 @@
 #define NROWS 3
 #define NCOLS 3
+#define SCAN_INTERVAL 10  
 
-const int row_pins[NROWS] = {2, 3, 4};
-const int col_pins[NCOLS] = {5, 6, 7};
+const int row_pins[NROWS] = {2, 3, 4};  
+const int col_pins[NCOLS] = {5, 6, 7};  
 bool btn_pressed[NROWS][NCOLS] = {false};  
-int buttonState = 0;
-int lastButtonState = 0;
-unsigned long pressStartTime[NROWS][NCOLS] = {0}; 
+unsigned long pressStartTime[NROWS][NCOLS] = {0};  
+
+volatile int current_row = 0; 
+
+bool btn_state[NROWS][NCOLS] = {false};  
+
+int pressed_buttons[NCOLS * NROWS];  
+int pressed_buttons_count = 0;  
+bool buttons_changed = false;  
 
 void setup() {
   for (int i = 0; i < NROWS; i++) {
     pinMode(row_pins[i], OUTPUT);
+    digitalWrite(row_pins[i], HIGH);  
   }
+
   for (int i = 0; i < NCOLS; i++) {
     pinMode(col_pins[i], INPUT_PULLUP);
   }
+
   Serial.begin(9600);
+  setupTimer();  
 }
 
 void loop() {
-  update_button_state();
-  delay(50);
+  delay(50);  
+
+  if (buttons_changed) {
+    print_pressed_buttons();
+    buttons_changed = false;  
+  }
+}
+
+
+void setupTimer() {
+  cli();  
+
+  TCCR1A = 0;  
+  TCCR1B = 0;
+
+  TCCR1B |= (1 << WGM12) | (1 << CS11) | (1 << CS10);  
+  OCR1A = (F_CPU / 64 / 1000) * SCAN_INTERVAL - 1;  
+
+  TIMSK1 |= (1 << OCIE1A);  
+  sei();  
+}
+
+ISR(TIMER1_COMPA_vect) {
+  scanRow();
+}
+
+void scanRow() {
+  for (int i = 0; i < NROWS; i++) {
+    PORTD |= (1 << row_pins[i]);
+  }
+  
+  PORTD &= ~(1 << row_pins[current_row]);
+  update_button_state();  
+  current_row = (current_row + 1) % NROWS;
 }
 
 void update_button_state() {
-  int newState = 0;
-  for (int row = 0; row < NROWS; row++) {
-    for (int i = 0; i < NROWS; i++) {
-      digitalWrite(row_pins[i], HIGH);
+  uint8_t colState = PIND;  
+
+  for (int col = 0; col < NCOLS; col++) {
+    bool isPressed = !(colState & (1 << col_pins[col]));  
+
+    if (isPressed && !btn_pressed[current_row][col]) {
+      btn_pressed[current_row][col] = true;
+      pressStartTime[current_row][col] = millis();
+      btn_state[current_row][col] = true;  
+      add_pressed_button(current_row, col);  
+      buttons_changed = true;  
+      Serial.print("Button ");
+      Serial.print(current_row * NCOLS + col + 1);
+      Serial.print(" pressed at ");
+      Serial.print(pressStartTime[current_row][col]);
+      Serial.println(" ms");
+    } 
+    else if (!isPressed && btn_pressed[current_row][col]) {
+      btn_pressed[current_row][col] = false;
+      unsigned long duration = millis() - pressStartTime[current_row][col];
+      btn_state[current_row][col] = false;   
+      remove_pressed_button(current_row, col);  
+      buttons_changed = true;  
+      Serial.print("Button ");
+      Serial.print(current_row * NCOLS + col + 1);
+      Serial.print(" held for ");
+      Serial.print(duration);
+      Serial.println(" ms");
     }
-    digitalWrite(row_pins[row], LOW);  
-    for (int col = 0; col < NCOLS; col++) {
-      bool isPressed = !digitalRead(col_pins[col]);  
-      if (isPressed && !btn_pressed[row][col]) {
-        pressStartTime[row][col] = millis();
-      } else if (!isPressed && btn_pressed[row][col]) {
-        unsigned long duration = millis() - pressStartTime[row][col];
-        Serial.print("duration of the button press ");
-        Serial.print(col + NCOLS * row + 1);
-        Serial.print(" - ");
-        Serial.print(duration);
-        Serial.print(" ms, beginning in ");
-        Serial.print(pressStartTime[row][col]);
-        Serial.println(" ms");
-      }
-      btn_pressed[row][col] = isPressed;
-      if (isPressed) {
-        newState |= (1 << (col + NCOLS * row));
-      }
-    }
-  }
-  if (newState != lastButtonState) {  
-    printButtons(newState);
-    lastButtonState = newState;
   }
 }
-void printButtons(int state) {
-  if (state == 0) {
-    Serial.println("No buttons pressed");
-    return;
+
+void add_pressed_button(int row, int col) {
+  if (pressed_buttons_count < NROWS * NCOLS) {
+    int button_index = row * NCOLS + col;
+    pressed_buttons[pressed_buttons_count] = button_index;  
+    pressed_buttons_count++;  
   }
-  Serial.print("The buttons are pressed: ");
-  for (int i = 0; i < 9; i++) {
-    if (state & (1 << i)) {
-      Serial.print(i + 1);
-      Serial.print(" ");
+}
+
+void remove_pressed_button(int row, int col) {
+  int button_index = row * NCOLS + col;
+  for (int i = 0; i < pressed_buttons_count; i++) {
+    if (pressed_buttons[i] == button_index) {
+      for (int j = i; j < pressed_buttons_count - 1; j++) {
+        pressed_buttons[j] = pressed_buttons[j + 1];
+      }
+      pressed_buttons_count--;  
+      break;
     }
   }
-  Serial.println();
+}
+
+void print_pressed_buttons() {
+  Serial.print("Pressed buttons: ");
+  if (pressed_buttons_count == 0) {
+    Serial.println("None");
+  } else {
+    for (int i = 0; i < pressed_buttons_count; i++) {
+      Serial.print(pressed_buttons[i] + 1);  
+      if (i < pressed_buttons_count - 1) {
+        Serial.print(", ");
+      }
+    }
+    Serial.println();
+  }
 }
